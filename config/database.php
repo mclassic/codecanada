93998da42051a480e0e750f06610913acb1263e4
Mike Classic <mike@classichalton.com>
2017-01-28 06:01:04 -0500

    database config

--- config/database.php
+++ config/database.php
@@ -46,8 +46,8 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
@@ -94,10 +94,10 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+        'cluster' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
