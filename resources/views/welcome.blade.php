c33b618cf0b2c90964cb6b42dc1df9c7d7416508
Mike Classic <mike@classichalton.com>
2017-01-31 22:11:06 -0500

    Landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,13 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
+                background-color: #3a3a3a;
+                color: #CACACA;
                 height: 100vh;
                 margin: 0;
             }
@@ -62,28 +60,25 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            a:link, a:active, a:visited {
+                color: #DEDEDE;
+            }
+
+            a:hover {
+                color: #BABABA;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <h1 class="title m-b-md">
+                    Code Canada
+                </h1>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/invite') }}">Join Our Slack Team</a>
                 </div>
             </div>
         </div>
